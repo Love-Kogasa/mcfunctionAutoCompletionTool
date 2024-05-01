51e4900d22784199d5c45ca3d0d14bccf19a6d87
Love-Kogasa <161302029+Love-Kogasa@users.noreply.github.com>
2024-05-01 11:33:57 +0800

    Create start.js

---
+++ Jss/start.js
@@ -0,0 +1,9 @@
+function start( mcf, outEle ){
+    if ( mcf == "" ){
+        outEle.innerHTML = "#文件不能为空哦w~"
+        return null
+    } else {
+        outEle.innerHTML = "#修改后的mcfunctions::\n" + startup( mcf ).cmds + "\n#共检查了 " + startup( mcf ).num + " 次"
+    }
+    return null
+}
